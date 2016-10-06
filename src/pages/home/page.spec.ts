77be40b14ee9db788b558703061ddb40c5f5c209
Fred Souza <fredericoamsouza@gmail.com>
2016-10-06 00:37:58 -0300

    Setting up karma + jasmine

---
+++ src/pages/home/page.spec.ts
@@ -0,0 +1,9 @@
+declare var describe, it, expect;
+
+describe("HomePage", () => {
+
+    it("should say 'Hello world!'", () => {
+
+        expect("Hello world!").toEqual("Hello world!");
+    });
+});
