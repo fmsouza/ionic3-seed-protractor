c504f2f41eb172b202e42497e5a486a8c2efabbf
Fred Souza <fredericoamsouza@gmail.com>
2017-10-19 14:59:39 -0200

    Implementing updated test engine

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { Page } from './app.po';
+
+describe('App', () => {
+  let page: Page;
+
+  beforeEach(() => {
+    page = new Page();
+  });
+
+  describe('default screen', () => {
+    beforeEach(() => {
+      page.navigateTo('/');
+    });
+
+    it('should have a title saying Home', () => {
+      page.getTitle().then(title => {
+        expect(title).toEqual('Home');
+      });
+    });
+  })
+});
---
+++ src/pages/home/spec.ts
@@ -0,0 +1,43 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { IonicModule, Platform, NavController } from 'ionic-angular/index';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { HomePage } from './index';
+import { PlatformMock, StatusBarMock, SplashScreenMock } from '../../../test/mocks-ionic';
+
+describe('HomePage', () => {
+  let de: DebugElement;
+  let comp: HomePage;
+  let fixture: ComponentFixture<HomePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HomePage],
+      imports: [
+        IonicModule.forRoot(HomePage)
+      ],
+      providers: [
+        NavController,
+        { provide: Platform, useClass: PlatformMock},
+        { provide: StatusBar, useClass: StatusBarMock },
+        { provide: SplashScreen, useClass: SplashScreenMock },
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePage);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h2'));
+  });
+
+  it('should create component', () => expect(comp).toBeDefined());
+
+  it('should have expected <h2> text', () => {
+    fixture.detectChanges();
+    const h2 = de.nativeElement;
+    expect(h2.innerText).toEqual("Hello world!", '<h2> should say something about "Hello world!"');
+  });
+});
