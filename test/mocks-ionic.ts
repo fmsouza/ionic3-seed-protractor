208078f37a0589c9ea41f4704967b031ede8aa42
Clevertech <clevertech@MacBook.local>
2017-11-07 15:51:13 -0200

    Updating ionic to 3.8.0

--- test/mocks-ionic.ts
+++ test/mocks-ionic.ts
@@ -2,6 +2,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 export class PlatformMock {
+
   public ready(): Promise<string> {
     return new Promise((resolve) => {
       resolve('READY');
@@ -9,14 +10,6 @@ export class PlatformMock {
   }
 
   public getQueryParam() {
-    return true;
-  }
-
-  public registerBackButtonAction(fn: Function, priority?: number): Function {
-    return (() => true);
-  }
-
-  public hasFocus(ele: HTMLElement): boolean {
     return true;
   }
 
@@ -28,37 +21,16 @@ export class PlatformMock {
     return true;
   }
 
-  public getElementComputedStyle(container: any): any {
-    return {
-      paddingLeft: '10',
-      paddingTop: '10',
-      paddingRight: '10',
-      paddingBottom: '10',
-    };
-  }
-
   public onResize(callback: any) {
     return callback;
-  }
-
-  public registerListener(ele: any, eventName: string, callback: any): Function {
-    return (() => true);
   }
 
   public win(): Window {
     return window;
   }
 
-  public raf(callback: any): number {
-    return 1;
-  }
-
   public timeout(callback: any, timer: number): any {
     return setTimeout(callback, timer);
-  }
-
-  public cancelTimeout(id: any) {
-    // do nothing
   }
 
   public getActiveElement(): any {
@@ -103,13 +75,5 @@ export class NavMock {
   public setRoot(): any {
     return true;
   }
-
-  public registerChildNav(nav: any): void {
-    return ;
-  }
-
-}
-
-export class DeepLinkerMock {
 
 }
