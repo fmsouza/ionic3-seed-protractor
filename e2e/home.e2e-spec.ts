9fb7b6dcd52e87e1e86f35e3621d5b48f5a8f5aa
Fred Souza <fredericoamsouza@gmail.com>
2017-11-08 21:58:23 -0200

    Refactoring example e2e test

--- e2e/home.e2e-spec.ts
+++ e2e/home.e2e-spec.ts
@@ -1,23 +1,17 @@
-import { Page } from './app.po';
+import { browser, element, by } from 'protractor';
 
-describe('App', () => {
-  let page: Page;
-
-  beforeEach(() => {
-    page = new Page();
-  });
+describe('Home', () => {
 
   describe('default screen', () => {
-    //beforeEach(() => page.navigateTo('/'));
 
     it('should have a title saying Home', () => {
-      page.getTitle().then(title => {
+      browser.getTitle().then(title => {
         expect(title).toEqual('Ionic');
       });
     });
 
     it('should present the package name', () => {
-      page.find('#pkgname').getText().then(text => {
+      element(by.css('#pkgname')).getText().then(text => {
         expect(text).toEqual('io.ionic.seed');
       });
     });
