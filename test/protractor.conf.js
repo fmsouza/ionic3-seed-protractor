35ae02a4f7bffe2253f1133b4c2fc0313105b69e
Fred Souza <fredericoamsouza@gmail.com>
2017-11-08 22:03:54 -0200

    Moving e2e tests to inside src directory

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -5,32 +5,15 @@ exports.config = {
   */
   seleniumAddress: 'http://localhost:4723/wd/hub',
 
-  //specs: ['../e2e/*.e2e-spec.js'],
-  specs: ['../e2e/**/*.e2e-spec.ts'],
+  specs: ['../src/**/*.e2e.ts'],
 
-  /*
-  * Some capabilities must be set to make sure appium can connect to your device.
-  * platformVersion: this is the version of android
-  * deviceName: your actual device name
-  * browserName: leave this empty, we want protractor to use the embedded webview
-  * autoWebView: set this to true for hybrid applications
-  * app: the location of the apk (must be absolute)
-  capabilities: {
-    platformName: 'android',
-    platformVersion: '7.0',
-    deviceName: 'ce031713733918ed0c',
-    browserName: '',
-    autoWebview: true,
-    app: '/Users/clevertech/projects/ionic3-seed/platforms/android/build/outputs/apk/android-debug.apk'
-  },
-  */
   capabilities: require('../appium.capabilities'),
 
   baseUrl: 'http://10.0.2.2:8000',
 
   beforeLaunch: function() {
     require('ts-node').register({
-      project: 'e2e'
+      project: 'src'
     });
   },
 
